43e4beb0cb025129f0e83c3e6096d87fdadce4e8
roostalol <30282035+roostalol@users.noreply.github.com>
2017-07-20 01:54:20 -0400

    Add files via upload

--- CharacterGroup.cs
+++ CharacterGroup.cs
@@ -8,71 +8,87 @@ using System.Collections.Generic;
 
 namespace FF12RNGHelper
 {
-  public class CharacterGroup
-  {
-    protected List<Character> characters;
-    protected int index;
+	public class CharacterGroup
+	{
+	protected List<Character> characters;
+	protected int charIndex;
 
-    public CharacterGroup()
-    {
-      this.characters = new List<Character>();
-      this.index = 0;
-    }
+	public CharacterGroup()
+	{
+		this.characters = new List<Character>();
+		this.charIndex = 0;
+	}
 
-    public void ClearCharacters()
-    {
-      this.characters.Clear();
-    }
+	public void ClearCharacters()
+	{
+		this.characters.Clear();
+	}
 
-    public int CharacterCount()
-    {
-      return this.characters.Count;
-    }
+	public int CharacterCount()
+	{
+		return this.characters.Count;
+	}
 
-    public void AddCharacter(Character c)
-    {
-      this.characters.Add(c);
-    }
+	public void AddCharacter(Character c)
+	{
+		this.characters.Add(c);
+	}
 
-    public void ResetIndex()
-    {
-      this.index = 0;
-    }
+	public void ResetIndex()
+	{
+		this.charIndex = 0;
+	}
 
     public int GetIndex()
-    {
-      return this.index;
-    }
+	{
+		return this.charIndex;
+	}
 
     public void SetIndex(int i)
-    {
-      this.index = i;
-    }
+	{
+		this.charIndex = i;
+	}
+	
+	public void IncrimentIndex()
+	{
+		this.charIndex = (this.charIndex + 1) % this.CharacterCount();
+	}
 
-    public uint GetHealValue(uint rngValue)
-    {
-      uint healValue = this.characters[this.index].GetHealValue(rngValue);
-      if (this.index == this.characters.Count - 1)
-        this.index = 0;
-      else
-        ++this.index;
-      return healValue;
-    }
+	public uint GetHealValue(uint rngValue)
+	{
+		uint healValue = this.characters[this.charIndex].GetHealValue(rngValue);
+		IncrimentIndex();
+		/*if (this.charIndex == this.characters.Count - 1)
+		this.charIndex = 0;
+		else
+		++this.charIndex;*/
+		return healValue;
+	}
 
-    public uint HealMax()
+	public uint PeekHealValue(uint rngValue)
+	{
+		uint healValue = this.characters[this.charIndex].GetHealValue(rngValue);
+		/*if (this.index == this.characters.Count - 1)
+			this.index = 0;
+		else
+			++this.index;*/
+		return healValue;
+	}
+
+	public uint HealMax()
     {
-      return this.characters[this.index].HealMax();
+      return this.characters[this.charIndex].HealMax();
     }
 
     public uint HealMin()
     {
-      return this.characters[this.index].HealMin();
+      return this.characters[this.charIndex].HealMin();
     }
 
     public bool ValidateHealValue(uint value)
     {
-      if (value <= this.characters[this.index].HealMax())
-        return value >= this.characters[this.index].HealMin();
+      if (value <= this.characters[this.charIndex].HealMax())
+        return value >= this.characters[this.charIndex].HealMin();
       return false;
     }
   }
